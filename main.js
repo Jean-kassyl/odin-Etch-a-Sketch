7b068341b4d067963b2a71fb8305dc382cbb2088
Jean Kassyl <kassylmac@gmail.com>
2023-10-19 21:51:39 +0200

    handle removing old grid element created when changing the grid size

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-const container = document.getElementById("container")
-const range_input = document.getElementById("range_number")
 
-function create_grid(size){
-    for(i= 0; i < 16; i++){
-        for(j = 0; j < 16; j++){
+const range_input = document.getElementById("range_number")
+const range_label = document.querySelectorAll(".range_label")
+
+
+function generate_grid(size){
+    for(let i= 1; i <= size; i++){
+        for(let j = 1; j <= size; j++){
             let div = document.createElement('div');
             div.setAttribute("class", "grid");
+            let container = document.getElementById("container")
             container.appendChild(div)
         }
     }
@@ -13,16 +16,37 @@ function create_grid(size){
     
 }
 
-create_grid()
+function create_grid(s = 16){
+    range_label.forEach(label => {
+        label.textContent = s
+    })
 
-const grids = document.querySelectorAll('.grid');
-grids.forEach(grid => {
+    let container = document.getElementById("container")
+    if (container.children.length > 0){
+    Array.from(container.children).forEach(div => div.remove())
+}
+
+    generate_grid(s)
+
+
+    const grids = document.querySelectorAll('.grid');
+    grids.forEach(grid => {
     grid.addEventListener("mouseenter", handleBackground)
 })
+    
+container.style.gridTemplateColumns = `repeat(${s}, 1fr)`
+container.style.gridTemplateRows = `repeat(${s}, 1fr)`
+
+    
+}
+
+create_grid()
+
+
 
 
 function handleBackground(e) {
-    let target = e.target;
+    let target = e.currentTarget;
     target.style.backgroundColor = "black"
 }
 
@@ -31,5 +55,8 @@ function handleBackground(e) {
 range_input.addEventListener("input", changeGridSize)
 
 function changeGridSize(e) {
+    let size = parseInt(e.target.value)
+    
+    create_grid(size)
     
 }
