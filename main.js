59aea711b5f623b107eaf5f585713f054b3ed190
Jean Kassyl <kassylmac@gmail.com>
2023-10-20 20:55:08 +0200

    work on the general design and add some feature (make it possible to use a randomized color grid)

--- main.js
+++ main.js
@@ -2,9 +2,34 @@ let container = document.getElementById("container")
 const range_input = document.getElementById("range_number")
 const range_label = document.querySelectorAll(".range_label")
 const clear = document.querySelector(".clear")
+const blackBtn = document.querySelector(".black_mode")
+const rainbowBtn = document.querySelector(".rainbow_mode")
 
 
 
+create_grid()
+
+
+
+blackBtn.classList.add("black")
+let color_mode = "black"
+
+blackBtn.addEventListener("click", () => {
+    color_mode = "black"
+    blackBtn.classList.add("black")
+    rainbowBtn.classList.remove("black")
+    
+})
+
+rainbowBtn.addEventListener("click", () => {
+    color_mode = "rainbow"
+    blackBtn.classList.remove("black")
+    rainbowBtn.classList.add("black")
+    
+})
+
+
+// clearing the grid when the user click the clear button
 clear.addEventListener("click", clearGrid)
 
 function clearGrid() {
@@ -14,7 +39,12 @@ function clearGrid() {
     })
 }
 
+// update grid
+range_input.addEventListener("input", changeGridSize)
 
+
+
+// helper function or callbacks
 
 function generate_grid(size){
     for(let i= 1; i <= size; i++){
@@ -33,40 +63,33 @@ function create_grid(s = 16){
         label.textContent = s
     })
 
-    
     if (container.children.length > 0){
-    Array.from(container.children).forEach(div => div.remove())
-}
+        Array.from(container.children).forEach(div => div.remove())
+    }
 
     generate_grid(s)
 
 
     const grids = document.querySelectorAll('.grid');
-    grids.forEach(grid => {
-    grid.addEventListener("mouseenter", handleBackground)
-})
+        grids.forEach(grid => {
+        grid.addEventListener("mouseenter", handleBackground)
+    })
     
-container.style.gridTemplateColumns = `repeat(${s}, 1fr)`
-container.style.gridTemplateRows = `repeat(${s}, 1fr)`
-
-    
+    container.style.gridTemplateColumns = `repeat(${s}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${s}, 1fr)`  
 }
-
-create_grid()
-
-
-
 
 function handleBackground(e) {
-    const colorWheel = Math.floor(Math.random() * 360) + 1
     let target = e.currentTarget;
-    target.style.backgroundColor = `hsl(${colorWheel}, 100%, 0%)`
-    console.log(colorWheel)
+    if (color_mode === "black"){
+        target.style.backgroundColor = "black"
+        
+    }
+    else {
+        const colorWheel = Math.ceil(Math.random() * 360) + 1
+        target.style.backgroundColor = `hsl(${colorWheel}, 100%, 50%)`
+    }   
 }
-
-
-
-range_input.addEventListener("input", changeGridSize)
 
 function changeGridSize(e) {
     let size = parseInt(e.target.value)
